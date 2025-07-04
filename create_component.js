6ab261f9483b25dec43f1e264c9ecdeebbc32926
rifqi856 <rifqifahmi680@gmail.com>
2025-07-05 06:23:56 +0700

    commit2

---
+++ create_component.js
@@ -0,0 +1,49 @@
+// ! Membuat component
+
+// * Fungsional Component
+
+// ? Membuat component menggunakan bentuk function (Functional Component) merupakan cara yang paling umum digunakan saat ini.
+
+
+// ^Berikut adalah skema dasar pembuatan functional component:
+
+function NamaComponent() {
+    return(
+        <div>
+            {/* isi kontent */}
+        </div>
+    );
+}
+
+export default NamaComponent;
+
+// cth penggunaan component
+
+
+
+ function App1() {
+    return (
+      <div>
+        <h1> Halo Semua!</h1>
+        <p>Selamat datang</p>
+
+       
+      </div>
+    )
+ }
+
+ function MyFooter() {
+  return(
+    <footer>
+      <p>By Terra</p>
+    </footer>
+  )
+ }
+
+//  ! Class Component
+
+// cth
+
+
+
+
